a34b6a2d97401fdb054c390637ddbaf618806c91
Brooke Wolford <bwolford@umich.edu>
2022-09-21 22:37:52 +0200

    Update ForestPlot.R

--- Day5/ForestPlot.R
+++ Day5/ForestPlot.R
@@ -10,22 +10,26 @@ df<-rbind(glgc[glgc$SNPID=="19:44897490:A:T",],
           bbj[bbj$SNPID=="19:44897490:A:T",],
           hunt[hunt$SNPID=="19:44897490:A:T",],fill=TRUE)
 df<-cbind(df,data.frame(study=c("GLGC","BBJ","HUNT")))
+
 #get pooled beta estimate
 df$w<-1/(df$SE*df$SE)
 df$wB<-df$w*df$BETA
 beta_pooled<-sum(df$wB)/sum(df$w)
 beta_pooled_se<-sqrt(1/sum(df$w))
 
+#read in meta-analysis data
 df2<-meta[meta$MarkerName=="19:44897490:A:T",]
 df2$study<-"meta-analysis"
 df2$BETA<-beta_pooled
 df2$SE<-beta_pooled_se
 df<-rbind(df,df2,fill=TRUE)
 
+#make 95% Confidence intervals
 df$UB<-df$BETA+(1.96*df$SE)
 df$LB<-df$BETA-(1.96*df$SE)
 df<-df%>%mutate(study= fct_relevel(study,"meta-analysis","HUNT","BBJ","GLGC") )
 
+#make plot
 pdf(file="ForestPlot.pdf",height=4,width=5)
 ggplot(df,aes(x=BETA,y=study,color=study)) + geom_point() + theme_bw() +
   geom_vline(xintercept=0,linetype="dashed",color="red") +
